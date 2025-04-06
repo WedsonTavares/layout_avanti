5f5725547a5498169ad566a5e658bd40a04cf480
Wedson Tavares <wedsonsobral@gmail.com>
2025-04-05 21:48:55 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 function toggleMenu() {
-    const categorias = document.getElementById('categorias');
-    categorias.classList.toggle('show'); // Adiciona ou remove a classe "show"
+    const menu = document.getElementById('menu-items');
+    const toggleButton = document.getElementById('menu-toggle');
+
+    // Alterna a visibilidade do menu
+    menu.classList.toggle('hidden');
+
+    // Atualiza o atributo aria-expanded para acessibilidade
+    const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
+    toggleButton.setAttribute('aria-expanded', !isExpanded);
 }
